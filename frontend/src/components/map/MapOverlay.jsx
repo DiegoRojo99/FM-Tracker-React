2cb5970fdc3069f8a7c0d53e4ba8e0bc4229a55f
DiegoRojo99 <diego99rojo@gmail.com>
2024-03-25 23:57:54 +0000

    Added trophy to overlay

--- frontend/src/components/map/MapOverlay.jsx
+++ frontend/src/components/map/MapOverlay.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import CrossIcon from './../../img/Cross.svg'
+import CrossIcon from './../../img/Cross.svg';
+import TrophyIcon from './../../img/Trophy.svg';
 
 export default function MapOverlay({competitions}){
 
@@ -26,11 +27,12 @@ export default function MapOverlay({competitions}){
   )
 }
 
-function MapOverlayRow({competition}){
+function MapOverlayRow({competition, achieved = false}){
 
   return (
     <div className="map-row">
       <p>{competition.name}</p>
+      <img className={achieved ? "trophy-icon white-image" : "trophy-icon"} src={TrophyIcon} />
     </div>
   )
 }
