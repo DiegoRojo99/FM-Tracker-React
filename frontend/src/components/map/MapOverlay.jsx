12f23e1863d8e2a35c5b20f83bcb7e0a85c22dbf
DiegoRojo99 <diego99rojo@gmail.com>
2024-03-25 23:30:43 +0000

    Map update leagues

---
+++ frontend/src/components/map/MapOverlay.jsx
@@ -0,0 +1,21 @@
+
+export default function MapOverlay({competitions}){
+  return (
+    <div className='map-overlay' >
+      {competitions && competitions.length ? 
+        <>
+          {competitions.map(l => <MapOverlayRow competition={l} />)}
+        </> : 
+      <></>}
+    </div>
+  )
+}
+
+function MapOverlayRow({competition}){
+
+  return (
+    <div className="map-row">
+      <p>{competition.name}</p>
+    </div>
+  )
+}
--- frontend/src/components/map/TrophiesMap.jsx
+++ frontend/src/components/map/TrophiesMap.jsx
@@ -2,23 +2,52 @@ import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import countriesData from './countries.geo.json';
-
+import './Map.css'
+import MapOverlay from './MapOverlay';
 
 function Map() {
 
-  const [leagues, setLeagues] = useState([]);
+  const [competitions, setCompetitions] = useState([]);
+  const [allCompetitions, setAllCompetitions] = useState([]);
+
+  useEffect(() => {
+    const fetchCompetitionsData = async () => {
+      try {
+        // Fetch competitions from backend when component mounts
+        const response = await fetch('http://localhost:5000/api/competitions/group')
+        if (!response.ok) {
+          throw new Error('Failed to fetch competitions');
+        }
+        const competitionsData = await response.json();
+        setAllCompetitions(competitionsData);
+      } catch (error) {
+        console.error('Error fetching competitions:', error);
+      }
+    };
   
+    fetchCompetitionsData();
+  }, []);
+
   useEffect(() => {
     // Initialize map
-    const map = L.map('map').setView([0, 0], 2);
+    const map = L.map('map', {
+      minZoom: 2,
+      maxZoom: 100
+    }).setView([0, 0], 2);
     var countryJson;
     
     function zoomToFeature(e) {
-      if(e?.target?.feature?.properties?.leagues){
-        setLeagues(e.target.feature.properties.leagues);
+      if(e?.target?.feature?.properties?.name){
+        const country = allCompetitions.find(c => c.country_name === e.target.feature.properties.name);
+        if(country?.competitions){
+          setCompetitions(country.competitions);
+        }
+        else{
+          setCompetitions([])
+        }
       }
       else{
-        setLeagues([])
+        setCompetitions([])
       }
       map.fitBounds(e.target.getBounds());
     }
@@ -57,19 +86,14 @@ function Map() {
       // Clean up map when component unmounts
       map.remove();
     };
-  }, []); // Empty dependency array ensures this effect runs only once
+  }, [allCompetitions]); 
+  
 
   return (
-    <>
-    <div id="map" style={{ height: '400px', width: '95%', margin: '16px auto' }}></div>
-    <div style={{ height: '128px', width: '95%', margin: '0 auto', backgroundColor: 'white' }}>
-      {leagues && leagues.length ? 
-      <>
-        {leagues.map(l => <p style={{ margin: '16px 32px' }}>{l}</p>)}
-      </> : 
-      <></>}
+    <div style={{height: '90%', width: '100%'}}>
+    <div id="map" style={{ height: '95%', width: '95%', margin: '16px auto' }}></div>
+      <MapOverlay competitions={competitions} />
     </div>
-    </>
   );
 }
 
