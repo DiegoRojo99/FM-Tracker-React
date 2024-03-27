5f1b9c4c7a32115e8628f3a02435a786f6064d80
DiegoRojo99 <diego99rojo@gmail.com>
2024-03-27 22:41:15 +0000

    Deleted Dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/home/Home';
 import './App.css';
-import Dev from './components/dev/Dev';
 import Map from './components/map/TrophiesMap';
 import LoginRegister from './components/login/LoginRegister';
 import { onAuthStateChanged } from '@firebase/auth';
@@ -23,9 +22,8 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/developer" element={<Layout><Dev /></Layout>} />
-      <Route path="/map" element={<Layout><Map /></Layout>} />
       <Route path="/" element={<Layout><Home /></Layout>} />
+      <Route path="/map" element={<Layout><Map /></Layout>} />
       <Route path="/login" element={<Layout><LoginRegister /></Layout>} />
     </Routes>
   );
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -7,12 +7,8 @@ function Nav() {
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/developer">Dev</Link></li>
         <li><Link to="/map">Map</Link></li>
         <li><Link to="/login">Login</Link></li>
-        {/* <li><Link to="/competitions/view">View Competitions</Link></li>
-        <li><Link to="/challenges/add">Add Challenge</Link></li>
-        <li><Link to="/challenges/view">View Challenges</Link></li> */}
       </ul>
     </nav>
   );
